5d433a95dad64c0dc3bf16da611c1c5e3b2872a2
nayHw <scm.nayhlaingwin@gmail.com>
2022-03-29 13:34:34 +0630

    [post-create] post create function

---
+++ vue-frame-master/src/services/post/post-create.js
@@ -0,0 +1,53 @@
+
+import axios from "axios";
+export default {
+  data: () => ({
+    dialog: false,
+    valid: true,
+    message: "",
+    titleRules: [
+      v => !!v || "Titile is required",
+      v => (v && v.length > 20) || "title must be greater than 20 characters"
+    ],
+    descriptionRules: [
+      v => !!v || "Description is required",
+      v =>
+        (v && v.length > 50) || "description must be greater than 50 characters"
+    ],
+    newPost: {
+      title: "",
+      description: "",
+      created_user_id: "",
+      created_user_name: ""
+    }
+  }),
+  mounted() {
+    this.newPost.created_user_id = this.$store.getters.userId;
+    this.newPost.created_user_name = this.$store.getters.userName;
+  },
+  methods: {
+    clear() {
+        this.newPost.title = "";
+        this.newPost.description = "";
+    },
+    dialogBox() {
+      this.dialog = true;
+    },
+    Cancel(){
+        this.dialog = false;
+    },
+    addPost() {
+      var input = this.newPost;
+      axios.post("/posts/create", input).then(() => {
+        this.newPost = {
+          title: "",
+          description: "",
+          created_user_id: "",
+          created_user_name: ""
+        };
+        this.$router.push("/post/list");
+      });
+    }
+  }
+};
+
